1c622d1534791966b119880fe9f5c9b5b4fcfb64
Pernilla Lindström <pernilla_lindstrom@outlook.com>
2022-08-26 11:21:30 +0200

    Added package lock

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log('Server running on port 3005');
+});
+
+app.get('/', (req, res) => {
+    res.send('server up and running');
+});
