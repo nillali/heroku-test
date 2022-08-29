5173291a44a3244a7ffba6c4b0f6a4ee52cbf488
Anna Toll <anna.toll@protonmail.com>
2022-08-29 12:29:01 +0200

    connected to mongodb database

---
+++ models/user.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    phone: {
+        type: String,
+        required: true,
+    },
+    pwd: {
+        type: String,
+        required: true,
+    },
+    accessLevel: {
+        type: String,
+        required: true,
+    }
+})
+
+const User = mongoose.model('user', userSchema)
+
+module.exports = User
--- server.js
+++ server.js
@@ -1,12 +1,29 @@
 const express = require('express');
+require('dotenv').config()
+const mongoose = require('mongoose');
+const User = require('./models/user')
 const app = express();
+
+const dbURI = process.env.dbURI
 
 const PORT = process.env.PORT || 3000
 
-app.listen(PORT, () => {
-    console.log('Server running on port 3005');
-});
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}, and connected to db`)
+        });
+    })
+    .catch((err) => console.log(err))
+
+app.use(express.json());
+
 
 app.get('/', (req, res) => {
-    res.send('server up and running');
+    User.findById('630c7c0587b93b3d0b16977e')
+        .then(result => {
+            res.send(result.name)
+        })
+        .catch(err => console.log(err))
+    // res.send('server up and running');
 });
