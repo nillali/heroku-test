b340e33a86ce564ffa4a3ebd09bfc554a6e72d1e
Isabelle <isabelle.annie.karlsson@gmail.com>
2022-09-16 14:09:49 +0200

    Refactoring 001: made more routes

--- server.js
+++ server.js
@@ -6,12 +6,19 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerJsDoc = require('swagger-jsdoc');
 const swaggerOptions = require('./swagger.json');
 
+const registerRoute = require("./routes/register.js");
+const userRoute = require("./routes/users.js");
+const loginRoute = require("./routes/login.js");
+
+// const allUsers = "/api/v0/users";
+// const authorizeUser = "/api/v0/authorize";
+// const registerUser = "/api/v0/register";
 
 // const { users } = require('./db')
 const app = express();
 
 app.use(cors());
-app.use('/', require('./routes/api.js'));
+
 const dbURI = process.env.dbURI
 const PORT = process.env.PORT || 3001
 
@@ -28,5 +35,9 @@ app.use(express.json());
 const swaggerDocument = swaggerJsDoc(swaggerOptions);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+
+app.use("/api/v0/register", registerRoute);
+app.use("/api/v0/users", userRoute);
+app.use("/api/v0/authorize", loginRoute);
 
 module.exports = app;
